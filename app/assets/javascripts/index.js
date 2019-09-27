dddc23f79629275b0afae29b442334b525f2c86f
nakaimomoko <mommkoman.sanjo.0318@icloud.com>
2019-09-27 11:32:40 +0900

    updating-messages

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -24,7 +24,7 @@ $(document).on('turbolinks:load', function(){
                     <p class='chat-group-user__name'>${name}</p>
                     <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
                 </div>`
-    member_list.append(html);
+                member_list.append(html);
   }
 
   $("#user-search-field").on("keyup", function() {
